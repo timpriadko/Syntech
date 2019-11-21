63ea97fbdd6432566a8004ab89ea4d94db08497c
SeryogaGitHub <main.email.seryoga@gmail.com>
2019-11-21 17:01:38 +0200

    continuation index page build

--- app/js/scripts.js
+++ app/js/scripts.js
@@ -32,6 +32,13 @@ $(document).ready(function() {
     nextArrow: '<button type="button" class="slick-next"><svg width="16" height="14" viewBox="0 0 16 14" fill="currentColor" stroke="none" xmlns="http://www.w3.org/2000/svg"><path d="M0 8L0 6L12 6L8 2L9 0L16 7L9 14L8 12L12 8L0 8Z"/></svg></button>'
   });
 
+  $('.slider-case').slick({
+    vertical: true,
+    dots: true,
+    verticalSwiping: true,
+    adaptiveHeight: true
+  });
+
   $.fn.forceNumbericOnly = function () {
     return this.each(function () {
       $(this).keydown(function (e) {
